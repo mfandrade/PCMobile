298ce1f91c38dc26cacdcff3b946f916205607e9
Marcelo F Andrade <mfandrade@gmail.com>
2014-05-05 13:16:45 -0300

    Hardcoded working formatter

--- src/org/tasafo/mobile/palestrascoletivas/EventActivity.java
+++ src/org/tasafo/mobile/palestrascoletivas/EventActivity.java
@@ -1,5 +1,8 @@
 package org.tasafo.mobile.palestrascoletivas;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,7 +20,8 @@ public class EventActivity extends Activity {
 	Event event = (Event) intent.getExtras().get("selected");
 	((TextView) findViewById(R.id.event_name)).setText(event.getName());
 	((TextView) findViewById(R.id.event_place)).setText(event.getPlace());
-	((TextView) findViewById(R.id.event_date)).setText(event.getDate().toString());
+	SimpleDateFormat extense = new SimpleDateFormat("d 'de' MMMM 'de' yyyy", Locale.getDefault());
+	((TextView) findViewById(R.id.event_date)).setText(extense.format(event.getDate()));
 	((TextView) findViewById(R.id.event_description)).setText(event.getDescription());
 
 	// getSupportActionBar().setUpAsHomeEnabled();
