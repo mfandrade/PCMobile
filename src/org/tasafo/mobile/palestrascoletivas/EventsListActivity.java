0449bd5b86a4689a1a8242041d16314a3dd93b2d
Marcelo F Andrade <mfandrade@gmail.com>
2014-05-05 09:48:10 -0300

    Initial commit

---
+++ src/org/tasafo/mobile/palestrascoletivas/EventsListActivity.java
@@ -0,0 +1,13 @@
+package org.tasafo.mobile.palestrascoletivas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EventsListActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_events_list);
+	}
+}
