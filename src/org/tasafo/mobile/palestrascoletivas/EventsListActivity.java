8ef2e2bf3ec48e503310163affbe99e4ab07aea2
Marcelo F Andrade <mfandrade@gmail.com>
2014-05-05 11:57:16 -0300

    Sample event listener

--- src/org/tasafo/mobile/palestrascoletivas/EventsListActivity.java
+++ src/org/tasafo/mobile/palestrascoletivas/EventsListActivity.java
@@ -5,8 +5,12 @@ import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class EventsListActivity extends Activity {
 
@@ -19,6 +23,14 @@ public class EventsListActivity extends Activity {
 	ArrayAdapter<Event> adapter = new ArrayAdapter<Event>(this, android.R.layout.simple_list_item_1, list);
 	ListView eventsList = (ListView) findViewById(R.id.events_list);
 	eventsList.setAdapter(adapter);
+
+	eventsList.setOnItemClickListener(new OnItemClickListener() {
+
+	    @Override
+	    public void onItemClick(AdapterView<?> adapter, View view, int pos, long id) {
+		Toast.makeText(view.getContext(), adapter.getItemAtPosition(pos).toString(), Toast.LENGTH_SHORT).show();
+	    }
+	});
     }
 
     @Override
