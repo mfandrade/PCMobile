df288235bfcebb7dd4fae3eb349f3a759b0f9b22
Marcelo F Andrade <mfandrade@gmail.com>
2014-05-05 10:51:37 -0300

    Simple events list with fake data

---
+++ src/org/tasafo/mobile/palestrascoletivas/Event.java
@@ -0,0 +1,127 @@
+package org.tasafo.mobile.palestrascoletivas;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Event {
+
+    private String  _name;
+    private String  _edition;
+    private String  _description;
+    private Date    _startDate;
+    private Integer _days;
+    private String  _address;    // street + district + state + country
+
+    public Event() {
+    }
+
+    public Event(String name, String edition, String description, String date, Integer days, String address) {
+	setName(name);
+	setEdition(edition);
+	setDescription(description);
+	setDate(date);
+	setDays(days);
+	setAddress(address);
+    }
+
+    public Event(JSONObject json) throws IllegalArgumentException {
+
+	try {
+	    setName(json.getString("name"));
+	    setEdition(json.getString("edition"));
+	    setDescription(json.getString("description"));
+	    setDate(json.getString("start_date"));
+	    setDays(Integer.parseInt(json.getString("days")));
+	    String address = "";
+	    String street = json.getString("street");
+	    String district = json.getString("district");
+	    String state = json.getString("state");
+	    String country = json.getString("country");
+	    if (street != null) {
+		address = street;
+	    }
+	    if (district != null) {
+		address += ", " + district;
+	    }
+	    if (state != null) {
+		address += ", " + state;
+	    }
+	    if (country != null) {
+		address += ", " + country;
+	    }
+	    setAddress(address);
+
+	} catch (JSONException e) {
+	    throw new IllegalArgumentException(e.getMessage());
+	}
+    }
+
+    @Override
+    public String toString() {
+	return _name + " - " + _edition;
+    }
+
+    public String getName() {
+	return _name;
+    }
+
+    public void setName(String name) {
+	_name = name;
+    }
+
+    public String getEdition() {
+	return _edition;
+    }
+
+    public void setEdition(String edition) {
+	_edition = edition;
+    }
+
+    public String getDescription() {
+	return _description;
+    }
+
+    public void setDescription(String description) {
+	_description = description;
+    }
+
+    public Date getDate() {
+	return _startDate;
+    }
+
+    public void setDate(Date startDate) {
+	_startDate = startDate;
+
+    }
+
+    public void setDate(String ymd) {
+	SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+	try {
+	    _startDate = dateFormatter.parse(ymd);
+	} catch (ParseException e) {
+	    throw new IllegalArgumentException(e.getMessage());
+	}
+    }
+
+    public Integer getDays() {
+	return _days;
+    }
+
+    public void setDays(Integer days) {
+	_days = days;
+    }
+
+    public String getAddress() {
+	return _address;
+    }
+
+    public void setAddress(String address) {
+	_address = address;
+    }
+
+}
--- src/org/tasafo/mobile/palestrascoletivas/EventsListActivity.java
+++ src/org/tasafo/mobile/palestrascoletivas/EventsListActivity.java
@@ -1,13 +1,23 @@
 package org.tasafo.mobile.palestrascoletivas;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class EventsListActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_events_list);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_events_list);
+
+	List<Event> list = new FakeEventsDatasource().getAll();
+	ArrayAdapter<Event> adapter = new ArrayAdapter<Event>(this, android.R.layout.simple_list_item_1, list);
+	ListView eventsList = (ListView) findViewById(R.id.events_list);
+	eventsList.setAdapter(adapter);
+	
+    }
 }
