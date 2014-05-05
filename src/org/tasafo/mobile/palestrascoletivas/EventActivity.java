7e18d8d1e31c22933fd5dc246966e1a8119b5c2c
Marcelo F Andrade <mfandrade@gmail.com>
2014-05-05 13:05:35 -0300

    Add a menu for event activity

--- src/org/tasafo/mobile/palestrascoletivas/EventActivity.java
+++ src/org/tasafo/mobile/palestrascoletivas/EventActivity.java
@@ -3,6 +3,7 @@ package org.tasafo.mobile.palestrascoletivas;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.TextView;
 
 public class EventActivity extends Activity {
@@ -15,10 +16,16 @@ public class EventActivity extends Activity {
 	Intent intent = getIntent();
 	Event event = (Event) intent.getExtras().get("selected");
 	((TextView) findViewById(R.id.event_name)).setText(event.getName());
-	((TextView) findViewById(R.id.event_address)).setText(event.getAddress());
+	((TextView) findViewById(R.id.event_place)).setText(event.getPlace());
 	((TextView) findViewById(R.id.event_date)).setText(event.getDate().toString());
 	((TextView) findViewById(R.id.event_description)).setText(event.getDescription());
 
 	// getSupportActionBar().setUpAsHomeEnabled();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	getMenuInflater().inflate(R.menu.event, menu);
+	return true;
     }
 }
