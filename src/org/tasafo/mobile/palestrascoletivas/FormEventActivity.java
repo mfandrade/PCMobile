bde99d6ee9c70ba508be54cc0cbfdc849a2124d1
Marcelo F Andrade <mfandrade@gmail.com>
2014-05-06 12:12:30 -0300

    Add a form to insert new events

---
+++ src/org/tasafo/mobile/palestrascoletivas/FormEventActivity.java
@@ -0,0 +1,13 @@
+package org.tasafo.mobile.palestrascoletivas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FormEventActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_form_event);
+    }
+}
